4f831f384db7e532736c3c56f17ecbc135df9b60
朱骏杰 <ories.zhu@mochain.vip>
2020-06-27 13:41:33 +0800

    显示的告诉typescript我们声明的是什么函数，但是这种方法很蛋疼，建议用as

--- app2.ts
+++ app2.ts
@@ -1,16 +1,45 @@
-import express from "express";
+import {RequestHandler} from 'express-serve-static-core';
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.get('/', function (request, response) {
-  response.send('你好');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const a:RequestHandler = function (req, res, next) {
+  next(createError(404));
+};
+
+// catch 404 and forward to error handler
+app.use(a);
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-
-app.post('./',(request, response, next)=>{})
-
-const port = 4000;
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
-});
-
+module.exports = app;
